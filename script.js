d2d4fe67894f61e60c5dc0384df4265136a39ddf
Ahmed Alwedyan <awedyan838@gmail.com>
2026-02-16 09:23:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const roles = [
-  "Blue Team Instructor",
-  "Digital Forensics Specialist",
-  "Incident Response Expert",
-  "Threat Hunter"
+"Blue Team Instructor",
+"Digital Forensics Specialist",
+"Incident Response Expert",
+"Threat Hunter"
 ];
 
 let i = 0;
@@ -11,19 +11,19 @@ let current = "";
 let deleting = false;
 
 function type() {
-  current = roles[i];
-  document.querySelector(".typing").textContent = current.substring(0, j);
+current = roles[i];
+document.querySelector(".typing").textContent = current.substring(0, j);
 
-  if (!deleting && j < current.length) {
-    j++;
-  } else if (deleting && j > 0) {
-    j--;
-  } else {
-    deleting = !deleting;
-    if (!deleting) i = (i + 1) % roles.length;
-  }
+if (!deleting && j < current.length) {
+j++;
+} else if (deleting && j > 0) {
+j--;
+} else {
+deleting = !deleting;
+if (!deleting) i = (i + 1) % roles.length;
+}
 
-  setTimeout(type, 80);
+setTimeout(type, 80);
 }
 
 type();
