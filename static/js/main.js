97896e9e1816719688130a235f9904d10782d8ee
Ahmed Alwedyan <awedyan838@gmail.com>
2026-02-16 08:03:13 +0300

    Initialize 3D scene with stars and animation

---
+++ static/js/main.js
@@ -0,0 +1,30 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#bg') });
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({ color: 0x00ff88 });
+    const star = new THREE.Mesh(geometry, material);
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+    star.position.set(x, y, z);
+    scene.add(star);
+}
+Array(200).fill().forEach(addStar);
+
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+
+function animate() {
+    requestAnimationFrame(animate);
+    scene.rotation.x += 0.001;
+    scene.rotation.y += 0.0005;
+    renderer.render(scene, camera);
+}
+animate();
