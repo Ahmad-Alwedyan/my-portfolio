9d6e0fc7cb91ef57e9f627637c64572c1aebe508
Ahmed Alwedyan <awedyan838@gmail.com>
2026-02-16 09:27:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
-const roles = [
+const roles=[
 "Blue Team Instructor",
 "Digital Forensics Specialist",
 "Incident Response Expert",
 "Threat Hunter"
 ];
 
-let i = 0;
-let j = 0;
-let current = "";
-let deleting = false;
+let i=0,j=0,current="",deleting=false;
 
-function type() {
-current = roles[i];
-document.querySelector(".typing").textContent = current.substring(0, j);
+function type(){
+current=roles[i];
+document.querySelector(".typing").textContent=current.substring(0,j);
 
-if (!deleting && j < current.length) {
-j++;
-} else if (deleting && j > 0) {
-j--;
-} else {
-deleting = !deleting;
-if (!deleting) i = (i + 1) % roles.length;
+if(!deleting && j<current.length){j++;}
+else if(deleting && j>0){j--;}
+else{
+deleting=!deleting;
+if(!deleting)i=(i+1)%roles.length;
 }
-
-setTimeout(type, 80);
+setTimeout(type,70);
 }
-
 type();
